d1a0ab844eeda31ef53822f76b97ee9ee48974e0
cschow <cschow@ucsd.edu>
2020-02-09 19:30:16 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/android_parsetagram/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.example.android_parsetagram;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+
+    private TextInputLayout username, password;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.usernameTextview);
+        password = findViewById(R.id.passwordTextView);
+        login = findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(
+                        username.getEditText().getText().toString(),
+                        password.getEditText().getText().toString()
+                );
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "done: Login error", e);
+                    e.printStackTrace();
+                    return;
+                }
+                goMainActivity();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
